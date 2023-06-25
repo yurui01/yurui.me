68d593686de94f7b0054927cf2080b004f74b7c5
余瑞 <pazzitul@163.com>
2023-06-25 18:12:52 +0800

    update body css

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${ubuntu.className}`} suppressHydrationWarning>
       <head />
-      <body className="antialiased mb-40 flex flex-col max-w-7xl lg:mx-auto min-h-full w-full bg-white border-x border-neutral-100 dark:bg-zinc-900 dark:border-zinc-800">
+
+      <body className="w-full h-full bg-neutral-50 dark:bg-black">
         <Providers>
-          <Navbar />
-          <main className="max-w-5xl flex-auto min-w-0 mt-6 md:mt-0 flex flex-col justify-center px-2 md:px-0 w-full min-h-full mx-auto">
-            {children}
-          </main>
+          <div className="antialiased mb-40 flex flex-col max-w-7xl lg:mx-auto min-h-full w-full bg-white border-x border-neutral-100 dark:bg-zinc-900 dark:border-zinc-800">
+            <Navbar />
+            <main className="max-w-5xl flex-auto min-w-0 mt-6 md:mt-0 flex flex-col justify-center px-2 md:px-0 w-full min-h-full mx-auto">
+              {children}
+            </main>
+          </div>
         </Providers>
       </body>
     </html>
